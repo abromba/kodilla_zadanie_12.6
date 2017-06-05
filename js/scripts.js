cbce9a415ce471f630a6f593f716d49f9d7a3d25
Ania <Ania>
2017-06-05 09:21:03 +0200

    format

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
-var url = 'https://restcountries.eu/rest/v1/name/'; 
+var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
 
 $(document).ready(function() {
     $('#search').click(searchCountries);
 
-    function searchCountries() { 
-		var countryName = $('#country-name').val() || 'Poland'; 
-        $.ajax({ 
-        url: url + countryName, 
-        method: 'GET', 
-        success: showCountriesList 
-        }); 
+    function searchCountries() {
+        var countryName = $('#country-name').val() || 'Poland';
+        $.ajax({
+            url: url + countryName,
+            method: 'GET',
+            success: showCountriesList
+        });
     }
 })
 
 function showCountriesList(resp) { 
-    countriesList.empty(); 
-    resp.forEach(function(item) { 
+    countriesList.empty();
+    resp.forEach(function(item) {
         $('<li>').text(item.name).appendTo(countriesList);
-    }); 
+    });
 }
